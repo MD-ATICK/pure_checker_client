dc98fbaf8f90ddb1f75aa72e1bc809a37746a2d6
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-06-26 08:28:53 -0600

    howo

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { toast } from "react-toastify"
 
-const server = 'local'
+const server = 'prod'
 // const url = server === 'prod' ? 'https://pure-checker-server.onrender.com/api' : 'http://localhost:9999/api'
 const url = server === 'prod' ? 'https://pure-checker-server.vercel.app/api' : 'http://localhost:9999/api'
 export const checkerApi = axios.create({ baseURL: `${url}/v1/gmail` })
